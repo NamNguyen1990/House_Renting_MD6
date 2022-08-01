53a5c280d0f41918ec3799a926e6a49e233c5f20
dell <namxuan.nguyen1990@gmail.com>
2022-08-01 16:31:05 +0700

    Done show_listhome

---
+++ src/app/components/housedetail/housedetail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-housedetail',
+  templateUrl: './housedetail.component.html',
+  styleUrls: ['./housedetail.component.css']
+})
+export class HousedetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
