f9bb722221302b4c2f5680b21b91a67e6453f0fc
Le van son <levansoon>
2022-08-04 17:28:55 +0700

    xong update profile

--- src/app/components/update-password/update-password.component.ts
+++ src/app/components/update-password/update-password.component.ts
@@ -4,6 +4,7 @@ import {FormControl, FormGroup} from "@angular/forms";
 import firebase from "firebase/compat";
 import User = firebase.User;
 import {Router} from "@angular/router";
+import {AuthenticationService} from "../../services/authentication.service";
 
 @Component({
   selector: 'app-update-password',
@@ -14,42 +15,42 @@ export class UpdatePasswordComponent implements OnInit {
   id: any = localStorage.getItem('ID')
   user: User | any
   editForm: FormGroup = new FormGroup({
-    password : new FormControl(),
+    oldPassword : new FormControl(),
+    newPassword : new FormControl(),
     confirmPassword : new FormControl(),
   })
   constructor(private userService: UserService,
-              private router: Router) { }
+              private router: Router,
+              private authService: AuthenticationService) { }
 
   ngOnInit(): void {
-    this.userService.getUserProfile(this.id).subscribe(data => {
-        this.editForm.patchValue({
-          password : data.password,
-          confirmPassword: data.confirmPassword,
-        })
-        console.log(data)
-      },
-      error => {
-        console.log(error);
-      });
+
   }
   getUser(){
-    this.userService.getUserProfile(this.id).subscribe((data) =>{
-      this.user = data
-    })
-  }
-  editUser() {
     this.user = {
-      username: this.editForm.value.username,
       password: this.editForm.value.password,
       confirmPassword: this.editForm.value.confirmPassword,
-
     }
-    this.userService.updatePassword(this.id, this.user).subscribe(() => {
-      console.log('id',this.id)
-      this.router.navigate(["/"])
-      alert("Successful !!")
-    }, error => {
-      console.log(error)
-    })
+  }
+
+
+  editPass() {
+    if (this.editForm.value.oldPassword==localStorage.getItem('PASSWORD')){
+      console.log(localStorage.getItem('PASSWORD'))
+      if ((this.editForm.value.oldPassword !== this.editForm.value.newPassword) &&(this.editForm.value.newPassword === this.editForm.value.confirmPassword )){
+        console.log(this.editForm.value.newPassword)
+        this.userService.updatePassword(this.id, this.user).subscribe(() => {
+          alert('Đổi mật khẩu thành công');
+          this.editForm.reset();
+        }, err => {
+          console.log(err)
+        });
+      }else {
+        alert('Lỗi trong')
+      }
+    }else {
+      alert('Error ngoài')
+    }
+
   }
 }
