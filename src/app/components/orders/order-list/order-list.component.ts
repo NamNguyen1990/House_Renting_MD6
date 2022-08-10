841b359b61a9a963093f0ce9d9c463ba4798be02
tranhung0609.github.io <trvanhung.nhk.0609@gmail.com>
2022-08-10 10:31:01 +0700

    chưa xong phần order

--- src/app/components/orders/order-list/order-list.component.ts
+++ src/app/components/orders/order-list/order-list.component.ts
@@ -2,6 +2,8 @@
 import { Component, OnInit } from '@angular/core';
 import {OrderService} from "../../../services/order.service";
 import {Orderr} from "../../../models/orderr";
+import {NzModalRef, NzModalService} from "ng-zorro-antd/modal";
+import {NgToastService} from "ng-angular-popup";
 
 
 @Component({
@@ -15,7 +17,8 @@ export class OrderListComponent implements OnInit {
   p: number = 1;
   total: number = 0;
   constructor(private orderService : OrderService,
-              ) { }
+              private modal: NzModalService,
+              private toast: NgToastService) { }
 
   ngOnInit(): void {
     this.getAllOrders();
@@ -31,19 +34,33 @@ export class OrderListComponent implements OnInit {
   }
   deleteOrder(id:any) {
     console.log(id)
-    if (confirm("are you sure?")){
+    // if (confirm("are you sure?")){
       this.orderService.delete(id).subscribe(() => {
         this.getAllOrders()
       }, error => {
-        alert("Can't cancel ! Only cancel the rental 1 day before the start date")
+        // alert("Can't cancel ! Only cancel the rental 1 day before the start date")
       })
-    }
+    // }
   }
   pageChangeEvent(event: number) {
     console.log(event)
 
     this.p = event;
     this.getAllOrders();
+  }
+
+  confirmModal?: NzModalRef; // For testing by now
+
+  showConfirm(id:any): void {
+    this.modal.confirm({
+      nzTitle: 'Are you sure to delete?',
+      nzContent: '<b style="color: red;"></b>',
+      nzOkText: 'Yes',
+      nzOkType: 'primary',
+      nzOnOk: () => this.deleteOrder(id),
+      nzCancelText: 'No',
+      nzOnCancel: () => console.log("cancel")
+    });
   }
 
 
