64274d816920ee3a9125a5b47985023129e0aecb
tranhung0609.github.io <trvanhung.nhk.0609@gmail.com>
2022-08-11 00:02:03 +0700

    sửa localhosst

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   apiUrl : 'http://209.97.167.87:8888',
   firebaseConfig : {
     apiKey: "AIzaSyDH1qwPunlsyxF9B2jnV_oStoPNv6tQk84",
